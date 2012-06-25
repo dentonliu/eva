7d6ff9381918c8f448705122c277739f7bfe54ec
Denton Liu <liutan2008@gmail.com>
2012-06-25 18:09:56 +0800

    Add search bar in header

---
+++ wp-content/themes/iphoto/includes/event.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$("#category-all-link").mouseover(
+	function(){
+		$("#category-all-list").show();
+	});
+	
+	$("#category-all-link").mouseout(
+	function(){
+		$("#category-all-list").hide();
+	});
+});
